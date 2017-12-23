Fixed Unistochastic Model (uses Softmax); made computeStyle a class method on Dataset

Simulation/SinkProp/Dataset.py
@@ -59,10 +59,19 @@ class Dataset():
     for x in range(self.numCharacters):
       self.probZ.append([])
 
+  # For each labeler, compute style matrix S given parameterizing matrix A
+  def computeStyle(data):
+    for i in range(data.numLabelers):
+      A = data.Labelers[i].A # Current parameterizing matrix
+      data.Labelers[i].iterations = sink_norm( np.exp(A) )
+
+      I = data.Labelers[i].iterations
+      data.Labelers[i].style = I[len(I)-1] # Set style matrix
+
   # For a large alphabet this will print a lot - consider piping to a file
   def outputResults(self):
     
-    computeStyle(self)
+    self.computeStyle()
 
     for i in range(self.numLabelers): 
       print "Style[%d]:" % i

Simulation/SinkProp/EM.py
@@ -33,7 +33,7 @@ def EM(data):
 # Compute P(Z_j | L, S) given S computed in the last MStep
 def EStep(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   # NOTE: For numerical stability, instead of computing the product in the paper,
   #       we compute the sum of the logs and exponentiate the sum
@@ -65,7 +65,7 @@ def EStep(data):
 
 def computeQ(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   n = data.Labelers[0].style.shape[0]
   I = np.identity(n)
@@ -90,7 +90,7 @@ def computeQ(data):
 
 def compute_gradient(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   gradients = np.empty(0) # Array of i entries, 1xn^2 matrices, dQdA_i
   dQ_dS = dQdS(data)      # Array of i entries, 1xn^2 matrices, dQdS_i

Simulation/SinkProp/Labeler.py
@@ -15,19 +15,5 @@ class Labeler:
   def __init__(self, A):
     self.A = A
     self.iterations = sink_norm(np.exp(A)) # Comment out for no SP
-    #self.style = self.iterations[len(self.iterations)-1] # Comment out for no SP
-    #self.style = A + 0.01
-
-# For each labeler, compute style matrix S given parameterizing matrix A
-def computeStyle(data):
-  for i in range(data.numLabelers):
-
-    A = data.Labelers[i].A # Current parameterizing matrix
-    data.Labelers[i].iterations = sink_norm( np.exp(A) ) # Comment out for no SP
-
-    # Comment out for no SP
-    I = data.Labelers[i].iterations
-    data.Labelers[i].style = I[len(I)-1] # Set style matrix
-
-    # Only for running without SP
-    #data.Labelers[i].style = A + 0.01
+    self.style = self.iterations[len(self.iterations)-1]
+    

Simulation/Unistochastic/EM.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 
 from util.sinkhorn import *
+from util.softmax import *
 from Labeler import *
 
 THRESHOLD = 1e-3
@@ -34,7 +35,7 @@ def EM(data):
 # Compute P(Z_j | L, S) given S computed in the last MStep
 def EStep(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   # NOTE: For numerical stability, instead of computing the product in the paper,
   #       we compute the sum of the logs and exponentiate the sum
@@ -66,7 +67,7 @@ def EStep(data):
 
 def computeQ(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   n = data.Labelers[0].style.shape[0]
   I = np.identity(n)
@@ -91,27 +92,17 @@ def computeQ(data):
 
 def compute_gradient(data):
 
-  computeStyle(data)
+  data.computeStyle()
 
   gradients = np.empty(0) # Array of i entries, 1xn^2 matrices, dQdA_i
   dQ_dS = dQdS(data)      # Array of i entries, 1xn^2 matrices, dQdS_i
   n = data.Labelers[0].style.shape[0] # Dimension of matrices
 
   for i in range(data.numLabelers):
-    dQ_dA = dQ_dS[i]
-    
-    # mat is input to row_norm
-    mat = data.Labelers[i].iterations[1]
-    partial = gradient_step(mat, row_grad)
+    dQ_dA = dQ_dS[i] 
+    mat = data.Labelers[i].A
+    partial = gradient_step(mat, softmax_gradient)
     dQ_dA = np.dot(dQ_dA, partial)
-
-    # mat is input to my_relu
-    mat = data.Labelers[i].iterations[0]
-    partial = np.zeros(mat.shape) + EPSILON
-    partial[np.where(mat>0)] = 1 # Derivative of ReLU
-    partial = np.diag(np.ravel(partial)) # Derivative needs to be n^2 by n^2 (and will be 0 when indices don't align)
-    dQ_dA = np.dot(dQ_dA, partial)    
-
     gradients = np.append(gradients, dQ_dA)
 
   return gradients
@@ -172,7 +163,6 @@ def unpackX(x, data):
     data.Labelers[i].A = A.reshape((c,c))
 
 # Computes the gradient with respect to m, a matrix
-# grad_func is either "row_grad" or "col_grad", which compute the gradient of the row norm or column norm respectively
 # TODO This may be vectorizable; it is a non-trivial change as the indices need to be set correctly
 def gradient_step(m, grad_func):
   n = m.shape[0]

Simulation/Unistochastic/Labeler.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 from util.sinkhorn import *
-
-EPSILON = 0.01
+from Dataset import *
 
 class Labeler:
 
@@ -10,17 +9,4 @@ class Labeler:
   # Style is A row normalized
   def __init__(self, A):
     self.A = A
-    B = my_relu(A)
-    C = row_norm(B)
-    self.iterations = [A, B, C]
-    self.style = C
-
-# For each labeler, compute style matrix S given parameterizing matrix A
-def computeStyle(data):
-  for i in range(data.numLabelers):
-    A = data.Labelers[i].A
-    data.Labelers[i].style = row_norm(my_relu(A))
-
-# Modified ReLU which forces strict positivity
-def my_relu(m):
-  return np.maximum(m, EPSILON)
+    self.style = softmax(A)