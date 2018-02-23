4bc2a01e3d393a4eda778115b24c96653c9c6aef
Michael Giancola <mjgiancola@wpi.edu>
2018-02-23 02:08:56 -0500

    Generated new noisy segmentations

--- Data_Generator/genNoisy.py
+++ Data_Generator/genNoisy.py
@@ -30,13 +30,13 @@ def saveImage(A, filename):
       lbl = A[i][j]
 
       if   lbl ==  0: img[i,j] = AQUA
-      elif lbl ==  1: img[i,j] = GREEN
-      elif lbl ==  2: img[i,j] = YELLOW
+      elif lbl ==  1: img[i,j] = BLACK
+      elif lbl ==  2: img[i,j] = RED
       elif lbl ==  3: img[i,j] = PURPLE
-      # elif lbl ==  4: img[i,j] = YELLOW
-      # elif lbl ==  5: img[i,j] = GREEN
-      # elif lbl ==  6: img[i,j] = AQUA
-      # elif lbl ==  7: img[i,j] = PURPLE
+      elif lbl ==  4: img[i,j] = YELLOW
+      elif lbl ==  5: img[i,j] = PINK
+      elif lbl ==  6: img[i,j] = TAN
+      elif lbl ==  7: img[i,j] = KHAKI
       # elif lbl ==  8: img[i,j] = BLACK 
       # elif lbl ==  9: img[i,j] = PINK
       else:
@@ -46,36 +46,121 @@ def saveImage(A, filename):
   output = Image.fromarray(img, 'RGB')
   output.save(filename)
 
+def generateCouple():
+  im = skimage.io.imread("../ADE20K/Generated/couple/gt_seg.png", as_grey=True)
+  _, classIm = np.unique(im, return_inverse=True)
+
+  # Map classes to reduce 10 classes to 4
+  classIm[classIm==1] = 0
+  classIm[classIm==2] = 0
+  classIm[classIm==3] = 0
+  classIm[classIm==4] = 1
+  classIm[classIm==5] = 2
+  classIm[classIm==6] = 3
+  classIm[classIm==7] = 3
+  classIm[classIm==8] = 3
+  classIm[classIm==9] = 3
+  img = classIm.reshape(im.shape)
+  saveImage(img, '../Simulation/Tests/ImageSegmentation/Images/couple/gt_seg.png')
+
+  # Generate set of random permutations of <numCharacters> elements
+  perms = list(itertools.permutations(range(4)))
+  shuffle(perms)
+
+  for i in range(10):
+    permuted = np.empty(classIm.shape)
+    perm = perms[i]
+    j = 0
+    for lbl in classIm:
+      permuted[j] = perm[lbl]
+      j += 1
+    permuted = permuted.reshape(im.shape)
+    
+    modifiedIm = bleed(permuted, numBleeds = 128)
+    saveImage(modifiedIm, '../Simulation/Tests/ImageSegmentation/Images/couple/noisy%d.png' % i)
+
+def generateFlag():
+  im = skimage.io.imread("../ADE20K/Generated/flag/gt_seg.png", as_grey=True)
+  _, classIm = np.unique(im, return_inverse=True)
+
+  img = classIm.reshape(im.shape)
+  saveImage(img, '../Simulation/Tests/ImageSegmentation/Images/flag/gt_seg.png')
+
+  # Generate set of random permutations of <numCharacters> elements
+  perms = list(itertools.permutations(range(2)))
+  shuffle(perms)
+
+  for i in range(10):
+    permuted = np.empty(classIm.shape)
+    perm = perms[i%2]
+    j = 0
+    for lbl in classIm:
+      permuted[j] = perm[lbl]
+      j += 1
+    permuted = permuted.reshape(im.shape)
+    
+    modifiedIm = bleed(permuted, numBleeds = 128)
+    saveImage(modifiedIm, '../Simulation/Tests/ImageSegmentation/Images/flag/noisy%d.png' % i)
+
+def generateLight():
+  im = skimage.io.imread("../ADE20K/Generated/light/gt_seg.png", as_grey=True)
+  _, classIm = np.unique(im, return_inverse=True)
+
+  classIm[classIm==1] = 0
+  classIm[classIm==2] = 1
+  classIm[classIm==3] = 1
+  classIm[classIm==4] = 0
+
+  img = classIm.reshape(im.shape)
+  saveImage(img, '../Simulation/Tests/ImageSegmentation/Images/light/gt_seg.png')
+
+  # Generate set of random permutations of <numCharacters> elements
+  perms = list(itertools.permutations(range(2)))
+  shuffle(perms)
+
+  for i in range(10):
+    permuted = np.empty(classIm.shape)
+    perm = perms[i%2]
+    j = 0
+    for lbl in classIm:
+      permuted[j] = perm[lbl]
+      j += 1
+    permuted = permuted.reshape(im.shape)
+    
+    modifiedIm = bleed(permuted, numBleeds = 128)
+    saveImage(modifiedIm, '../Simulation/Tests/ImageSegmentation/Images/light/noisy%d.png' % i)
+
+def generatePeople():
+  im = skimage.io.imread("../ADE20K/Generated/people/gt_seg.png", as_grey=True)
+  _, classIm = np.unique(im, return_inverse=True)
+
+  classIm[classIm==3] = 2
+  classIm[classIm==4] = 2
+  classIm[classIm==5] = 2
+  classIm[classIm==6] = 3
+  classIm[classIm==7] = 3
+
+  img = classIm.reshape(im.shape)
+  saveImage(img, '../Simulation/Tests/ImageSegmentation/Images/people/gt_seg.png')
+
+  # Generate set of random permutations of <numCharacters> elements
+  perms = list(itertools.permutations(range(4)))
+  shuffle(perms)
+
+  for i in range(10):
+    permuted = np.empty(classIm.shape)
+    perm = perms[i]
+    j = 0
+    for lbl in classIm:
+      permuted[j] = perm[lbl]
+      j += 1
+    permuted = permuted.reshape(im.shape)
+
+    # Had to modify since the image is so small
+    modifiedIm = bleed(permuted, kernelSize=4, numBleeds = 128)
+    saveImage(modifiedIm, '../Simulation/Tests/ImageSegmentation/Images/people/noisy%d.png' % i)
+
+
 # This script generates randomly permuted noisy versions of an image
 if __name__ == "__main__":
-    im = skimage.io.imread("../ADE20K/Generated/gt_seg.png", as_grey=True)
-    _, classIm = np.unique(im, return_inverse=True)
-
-    # Map classes to reduce 10 classes to 4
-    classIm[classIm==1] = 0
-    classIm[classIm==2] = 0
-    classIm[classIm==3] = 0
-    classIm[classIm==4] = 1
-    classIm[classIm==5] = 2
-    classIm[classIm==6] = 3
-    classIm[classIm==7] = 3
-    classIm[classIm==8] = 3
-    classIm[classIm==9] = 3
-    img = classIm.reshape(im.shape)
-    saveImage(img, '../Simulation/Tests/ImageSegmentation/Images/gt_seg.png')
-
-    # Generate set of random permutations of <numCharacters> elements
-    perms = list(itertools.permutations(range(4)))
-    shuffle(perms)
-
-    for i in range(10):
-      permuted = np.empty(classIm.shape)
-      perm = perms[i]
-      j = 0
-      for lbl in classIm:
-        permuted[j] = perm[lbl]
-        j += 1
-      permuted = permuted.reshape(im.shape)
-      
-      modifiedIm = bleed(permuted, numBleeds = 128)
-      saveImage(modifiedIm, '../Simulation/Tests/ImageSegmentation/Images/noisy%d.png' % i)
+  # generatePeople()
